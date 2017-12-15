7ff76d970f2ddb82c91bb376bea8f68abf3dbaaf
Rodrigo Filomeno <rodrigofilomeno@Mac-mini-de-Rodrigo.local>
2017-12-15 17:11:24 -0200

    Login Facebook ok

--- app/src/main/java/com/example/filomeno/rm/at_desenvolvimento_rodrigo_filomeno/CONTROLLER/MainActivity.java
+++ app/src/main/java/com/example/filomeno/rm/at_desenvolvimento_rodrigo_filomeno/CONTROLLER/MainActivity.java
@@ -11,10 +11,20 @@ import android.widget.Toast;
 import com.example.filomeno.rm.at_desenvolvimento_rodrigo_filomeno.CONTROLLER.DAO.FirebaseDao;
 import com.example.filomeno.rm.at_desenvolvimento_rodrigo_filomeno.CONTROLLER.ENTITIES.User;
 import com.example.filomeno.rm.at_desenvolvimento_rodrigo_filomeno.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.Email;
@@ -28,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
     Validator validator;
     private FirebaseAuth FbAuth;
     private User user;
+    LoginButton loginFacebook;
+    private FirebaseAuth FirebaseAuth;
+    private CallbackManager mCallbackManager;
 
     @NotEmpty(message = "Campo nao pode ficar em branco.")
     @Email(message = "Coloque um e-mail válido.")
@@ -41,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
         validator = new Validator(this);
@@ -51,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
         LoginSenha = (EditText) findViewById(R.id.etSenhaLogin);
 
 
-
+        init();
 
     }
 
@@ -117,6 +131,51 @@ public void irParaTarefas(){
 
 
 }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void init() {
+        FirebaseAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
+
+        loginFacebook = (LoginButton) findViewById(R.id.btnLoginFacebook);
+        loginFacebook.setReadPermissions("email","public_profile");
+
+
+        loginFacebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+                startActivity(new Intent(getApplicationContext(),TarefasActivity.class));
+            }
+            @Override
+            public void onCancel() {
+            }
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),"Facebook login failed",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token){
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        FirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = FirebaseAuth.getCurrentUser();
+                        }else{
+
+                        }
+                    }
+                });
+    }
 
 
 }
